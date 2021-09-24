916ab650e0d892ddd22a233b820d8416543d745f
iremdgnc <irem.doganci@hotmail.com>
2021-09-24 13:04:12 +0300

    change add_product, delete read_file,get_current_product_ids, add delete_id,change function name

--- main.py
+++ main.py
@@ -3,36 +3,51 @@ import random
 
 
 def welcome_raf():
+    print("\nRaf'a Hoş Geldiniz.")
     while True:
-        print("\nRaf'a Hoş Geldiniz.")
-        a = input("- Mevcut Ürünleri Görmek İçin 'liste' Yazınız. \n- Yeni Ürün Eklemek İçin 'ekle' Yazınız. \n- "
-                  "Bir Ürün Silmek İçin 'sil' Yazınız.  \n- Çıkmak İçin Çıkış Yazınız. \n")
-        if a == ("liste" or "Liste"):
-            product_list_read()
-        elif a == ("ekle" or "Ekle"):
+        print("\nHangi İşlemi Yapmak İstersiniz? ")
+        user_request = input("- Mevcut Ürünleri Görmek İçin 'liste' Yazınız. \n- Yeni Ürün Eklemek İçin 'ekle' "
+                             "Yazınız. \n- Bir Ürün Silmek İçin 'sil' Yazınız.  \n- Çıkmak İçin Çıkış Yazınız. \n")
+        if user_request == ("liste" or "Liste"):
+            product_list()
+        elif user_request == ("ekle" or "Ekle"):
             add_product()
-        elif a == ("sil" or "Sil"):
-            delete_id()
-        elif a == ("çıkış" or "cikis" or "Çıkış"):
+        elif user_request == ("sil" or "Sil"):
+            delete_product()
+        elif user_request == ("çıkış" or "cikis" or "Çıkış"):
             print("Programdan çıkıldı.")
             break
 
 
-def get_id():
-    id_list = []
+def menu():
+    while True:
+        user_new_request = input(
+            "- Mevcut Ürünleri Görmek İçin 'liste' Yazınız. \n - Bir Ürün Silmek İçin 'sil' Yazınız.  \n- "
+            "Çıkmak İçin Çıkış Yazınız. \n")
+        if user_new_request == ("liste" or "Liste"):
+            product_list()
+        elif user_new_request == ("sil" or "Sil"):
+            delete_product()
+        elif user_new_request == ("çıkış" or "cikis" or "Çıkış"):
+            print("Programdan çıkıldı.")
+            break
+
+
+def get_product_id():
+    product_id_list = []
     if os.path.isfile("ürünler.txt"):
         with open("ürünler.txt", "r+") as file:
             next_line = file.readline()
             while next_line != "":
                 p_id = next_line.split(" ")[2]
-                id_list.append(p_id)
+                product_id_list.append(p_id)
                 next_line = file.readline()
-            return id_list
+            return product_id_list
     else:
-        return id_list
+        return product_id_list
 
 
-def product_list_read():
+def product_list():
     if os.path.isfile("ürünler.txt"):
         with open("ürünler.txt", "r") as file:
             print(file.read())
@@ -40,17 +55,17 @@ def product_list_read():
         print("Dosya Bulunamadı")
 
 
-def generate_id(array):
-    limit = 10
-    random_number = str(random.randint(1, limit))
+def generate_product_id(array):
+    range_limit = 10
+    random_number = str(random.randint(1, range_limit))
     while True:
         if random_number in array:
-            if len(array) == limit:
+            if len(array) == range_limit:
                 print("\n!!! Kapasite doldu. Kayıt yapılamaz. !!!")
                 print("Listeleme Veya Silme İşlemi Gerçekleştirebilirsiniz.")
-                welcome_raf()
+                menu()
             else:
-                random_number = str(random.randint(0, limit))
+                random_number = str(random.randint(0, range_limit))
 
         if random_number not in array:
             array.append(random_number)
@@ -58,15 +73,15 @@ def generate_id(array):
 
 
 def add_product():
-    product_id = (generate_id(get_id()))
+    product_id = (generate_product_id(get_product_id()))
     products = input("Product_Name:")
     with open("ürünler.txt", "a") as file:
         file.write(" Product_ID: " + product_id + " Product_Name: " + products + "\n")
         return file
 
 
-def delete_id():
-    product_list_read()
+def delete_product():
+    product_list()
     delete = input("Silmek İstediğiniz Ürüne Ait Product_Id Giriniz: ")
     with open("ürünler.txt", "r+") as file:
         new_f = file.readlines()
