d82c3ff634e89425b645dd55447c8b0624eb3a1e
iremdgnc <irem.doganci@hotmail.com>
2021-09-24 10:09:24 +0300

    change add_product, delete read_file,get_current_product_ids, add delete_id

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ def welcome_raf():
     while True:
         print("\nRaf'a Hoş Geldiniz.")
         a = input("- Mevcut Ürünleri Görmek İçin 'liste' Yazınız. \n- Yeni Ürün Eklemek İçin 'ekle' Yazınız. \n- "
-                  "Çıkmak İçin Çıkış Yazınız. \n")
+                  "Bir Ürün Silmek İçin 'sil' Yazınız.  \n- Çıkmak İçin Çıkış Yazınız. \n")
         if a == ("liste" or "Liste"):
             product_list_read()
         elif a == ("ekle" or "Ekle"):
             add_product()
+        elif a == ("sil" or "Sil"):
+            delete_id()
         elif a == ("çıkış" or "cikis" or "Çıkış"):
             print("Programdan çıkıldı.")
             break
@@ -35,7 +37,7 @@ def product_list_read():
         with open("ürünler.txt", "r") as file:
             print(file.read())
     else:
-        print("dosya bulunamadı")
+        print("Dosya Bulunamadı")
 
 
 def generate_id(array):
@@ -45,6 +47,7 @@ def generate_id(array):
         if random_number in array:
             if len(array) == limit:
                 print("\n!!! Kapasite doldu. Kayıt yapılamaz. !!!")
+                print("Listeleme Veya Silme İşlemi Gerçekleştirebilirsiniz.")
                 welcome_raf()
             else:
                 random_number = str(random.randint(0, limit))
@@ -55,11 +58,24 @@ def generate_id(array):
 
 
 def add_product():
-    products = input("Product_Name:")
     product_id = (generate_id(get_id()))
+    products = input("Product_Name:")
     with open("ürünler.txt", "a") as file:
         file.write(" Product_ID: " + product_id + " Product_Name: " + products + "\n")
         return file
+
+
+def delete_id():
+    product_list_read()
+    delete = input("Silmek İstediğiniz Ürüne Ait Product_Id Giriniz: ")
+    with open("ürünler.txt", "r+") as file:
+        new_f = file.readlines()
+        file.seek(0)
+        for line in new_f:
+            if ('Product_ID: ' + delete) not in line:
+                file.write(line)
+        print("Silme İşlemi Gerçekleştirildi.")
+        file.truncate()
 
 
 if __name__ == '__main__':
