b9383f951e1f78ecb533d6ea17c681a5302ce320
iremdgnc <irem.doganci@hotmail.com>
2021-09-28 10:44:34 +0300

    change get_element_by_name()

--- db.py
+++ db.py
@@ -1,31 +1,19 @@
 import json
 
 
-def Merge(dict2, dict1):
-    merge_dict = {**dict1, **dict2}
-    return merge_dict
+def get_element_by_name():
+    # Open JSON
+    with open("db.JSON", "r") as file:
+        data = json.load(file)
+
+    print(type(data['products']))
+
+    user_input = {"product_id": input('Product Id Giriniz: '), "product_name": input('Product Name Giriniz: ')}
+    data['products'].append(user_input)
+
+    with open("db.JSON", "w") as file:
+        json.dump(data, file)
 
 
-with open("db.JSON", "r") as file:
-    data = json.load(file)
-    dict1 = data
-
-
-dict2 = {
-
-        "product_id": input('Product Id Giriniz: '),
-        "product_name": input('Product Name Giriniz: ')
-
-}
-
-print(dict1)
-print(dict2)
-dict3 = Merge(dict1, dict2)
-print(dict3)
-
-new_dict = json.loads(dict3)
-print(new_dict)
-
-"""file = open("db.JSON", "w")
-json.dump(dict3, file)
-file.close()"""
+if __name__ == "__main__":
+    get_element_by_name()
